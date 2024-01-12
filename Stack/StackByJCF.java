19a031e3d27b53585bb35d7e626755cabace2632
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-01-12 10:41:38 +0530

    Stack in JCF

---
+++ Stack/StackByJCF.java
@@ -0,0 +1,18 @@
+package Stack;
+import java.util.*;
+
+public class StackByJCF {
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.size());
+        System.out.println(s.isEmpty());
+        System.out.println(s.peek());
+    }
+}

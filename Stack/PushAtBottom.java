605a55c4d823c8a8e137fba1842170e3ece8e5a4
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-01-12 10:38:38 +0530

    push at bottom in stack

---
+++ Stack/PushAtBottom.java
@@ -0,0 +1,26 @@
+package Stack;
+import java.util.*;
+
+public class PushAtBottom {
+
+    public static void pushAtbottom(Stack<Integer>s,Integer v){
+       if(s.isEmpty()){
+        s.push(v);
+        return;
+       }
+       Integer cur=s.pop();
+       pushAtbottom(s, v);
+       s.push(cur);
+    }
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        pushAtbottom(s,5);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}

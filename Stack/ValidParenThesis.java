34c336be7fa549f47a3a9207a0fc85f33c479619
satyam kumar shah <satyamshah1234567890@gmail.com>
2024-01-12 10:44:05 +0530

    ValidParenThesis

---
+++ Stack/ValidParenThesis.java
@@ -0,0 +1,27 @@
+package Stack;
+import java.util.*;
+
+public class ValidParenThesis {
+
+    public static boolean isValid(String str){
+        Stack<Character>s=new Stack<>();
+        for(char ch:str.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                s.push(ch);
+            }else{
+                if(!(s.peek()<ch&&s.peek()+3>=ch)){
+                    return false;
+                }
+                s.pop();
+            }
+        }
+        return s.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str="({[[(}{})]})";
+        String str2="{([({()(){}[{([]())}]})])}()[{}]";
+        System.out.println(isValid(str));
+        System.out.println(isValid(str2));
+    }
+}
